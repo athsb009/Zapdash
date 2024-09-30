115588a3e35a34280ab5e15793c6942de4047ccf
athsb009 <98641425+athsb009@users.noreply.github.com>
2024-09-30 00:08:42 -0500

    Update Appbar.tsx

--- frontend/components/Appbar.tsx
+++ frontend/components/Appbar.tsx
@@ -7,7 +7,7 @@ export const Appbar = () => {
     const router = useRouter();
     return <div className="flex border-b justify-between p-4">
         <div className="flex flex-col justify-center text-2xl font-extrabold">
-            Zapier
+            ZapDash
         </div>
         <div className="flex">
             <div className="pr-4">
